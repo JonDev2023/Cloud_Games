d9485a187de661220d5728525cfdeadb3c4f791d
JonDev2023 <roupasee@gmail.com>
2023-02-22 19:03:07 -0300

    Now I am Released

--- main.py
+++ main.py
@@ -1,16 +1,57 @@
 import os
 import random
 from urllib import request
+from unidecode import unidecode
 import app
-import smtplib
 
-if input('Change Sprite (S/N): ') == 'S':
+SYMBOLS = 'áãâàéêèíîìóõôòúûù'
+
+if input('Change Sprite (Y/N): ') == 'Y':
     app.cmd('mspaint', 'sprite.png')
 
 # Input variables
 server = input('Type the server: ')
 game_name = input('Type the game Name(maiuscle and minuscle is different): ')
-otimizate = input('Otimizate? (S/N): ')
+otimizate = input('Otimizate? (Y/N): ')
+
+SYMBOLS_LIST = []
+
+for i in SYMBOLS:
+    SYMBOLS_LIST.append(i)
+
+cripto_text1 = random.choice(SYMBOLS_LIST)
+cripto_text2 = random.choice(SYMBOLS_LIST)
+cripto_text3 = random.choice(SYMBOLS_LIST)
+
+cripto_text = cripto_text1 + cripto_text2 + cripto_text3
+print(f'type this without symbols: {cripto_text}')
+
+response1 = input('type the 1: ')
+response2 = input('type the 2: ')
+response3 = input('type the 3: ')
+
+index = 1
+note = 0
+max_note = 3
+for i in cripto_text:
+    if index == 1:
+        if response1 == unidecode(cripto_text1):
+            note += 1
+    if index == 2:
+        if response2 == unidecode(cripto_text2):
+            note += 1
+    if index == 3:
+        if response3 == unidecode(cripto_text3):
+            note += 1
+    index += 1
+
+if note > max_note:
+    note = max_note
+
+if note == max_note:
+    print('Acess Garanted')
+else:
+    exit('Acess not Allowed')
 
 # Downloading Main HTML File
 file_url = server + '/index.html'
@@ -37,7 +78,7 @@ except OSError:
     print('SKipped')
 
 # Optimize App
-if otimizate == 'S':
+if otimizate == 'Y':
     app.optimize()
 
 # Running App
